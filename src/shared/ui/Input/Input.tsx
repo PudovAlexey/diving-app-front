b839be5e7949beb5aed08523eef9e4ec05251b96
aa.pudov <aa.pudov@svo.aero>
2023-10-10 22:19:20 +0300

    fix[ECODE-140]: add

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,46 +1,46 @@
-import React from "react"
-import { Input as MuiInput, InputProps as MuiInputProps } from "@mui/material"
-import styled from "@emotion/styled"
-import { InputBySize } from "./InputBySize"
+import React from "react";
+import { Input as MuiInput, InputProps as MuiInputProps } from "@mui/material";
+import styled from "@emotion/styled";
+import { InputBySize } from "./InputBySize";
 import {
-	LabelComponent,
-	LabelComponentProps,
-} from "../labelComponent/LabelComponent"
+  LabelComponent,
+  LabelComponentProps,
+} from "../labelComponent/LabelComponent";
 
 export type InputProps = Omit<MuiInputProps, "size"> & {
-	size?: "56" | "44" | "32" | "24"
-	label?: LabelComponentProps["label"]
-	validationText?: LabelComponentProps["validationText"]
-	validationStatus?: LabelComponentProps["validationStatus"]
-}
+  size?: "56" | "44" | "32" | "24";
+  label?: LabelComponentProps["label"];
+  validationText?: LabelComponentProps["validationText"];
+  validationStatus?: LabelComponentProps["validationStatus"];
+};
 
 function Input(props: InputProps) {
-	const {
-		size,
-		label,
-		required,
-		validationText,
-		validationStatus,
-		...otherProps
-	} = props
-	const InputComponent = StyledInput(size)
+  const {
+    size,
+    label,
+    required,
+    validationText,
+    validationStatus,
+    ...otherProps
+  } = props;
+  const InputComponent = StyledInput(size);
 
-	return (
-		<LabelComponent
-			validationText={validationText}
-			validationStatus={validationStatus}
-			required={required}
-			label={label}
-		>
-			<InputComponent required={required} {...otherProps} />
-		</LabelComponent>
-	)
+  return (
+    <LabelComponent
+      validationText={validationText}
+      validationStatus={validationStatus}
+      required={required}
+      label={label}
+    >
+      <InputComponent required={required} {...otherProps} />
+    </LabelComponent>
+  );
 }
 
 const StyledInput = (size: InputProps["size"]) =>
-	styled(MuiInput)(({ theme }) => ({
-		...InputBySize(size),
-		color: theme.palette.primary.contrastText,
-	}))
+  styled(MuiInput)(({ theme }) => ({
+    ...InputBySize(size),
+    color: theme.palette.primary.contrastText,
+  }));
 
-export { Input }
+export { Input };
--- src/shared/ui/typography/index.ts
+++ src/shared/ui/typography/index.ts
@@ -1,23 +1,23 @@
-import { Body } from "./Body/Body"
-import { Heading } from "./Heading/Heading"
-import { Caption } from "./Caption/Caption"
-import { Button } from "./Button/Button"
+import { Body } from "./Body/Body";
+import { Heading } from "./Heading/Heading";
+import { Caption } from "./Caption/Caption";
+import { Button } from "./Button/Button";
 
-import { size as bodySize } from "./Body/body"
-import { size as captionSize } from "./Caption/caption"
-import { size as headingSize } from "./Heading/heading"
-import { size as buttonSize } from "./Button/button"
+import { size as bodySize } from "./Body/body";
+import { size as captionSize } from "./Caption/caption";
+import { size as headingSize } from "./Heading/heading";
+import { size as buttonSize } from "./Button/button";
 
 export const Typography = {
-	Body,
-	Heading,
-	Caption,
-	Button,
+  Body,
+  Heading,
+  Caption,
+  Button,
 
-	sizes: {
-		bodySize,
-		captionSize,
-		headingSize,
-		buttonSize,
-	},
-}
+  sizes: {
+    bodySize,
+    captionSize,
+    headingSize,
+    buttonSize,
+  },
+};
